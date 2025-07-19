c7cbc10a1cc60dfe0577542776c229bd8a0a483a
koena0912 <maitikoena@gmail.com>
2025-07-19 12:49:07 +0000

    Sync LeetCode submission - Palindrome Number (java)

---
+++ problems/palindrome_number/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n=x;
+        int revNum=0;
+        while(n>0){
+            int lastDigit= n%10; //remainder
+            revNum= revNum*10+lastDigit;
+            n=n/10;
+           
+        }
+        if(revNum==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
