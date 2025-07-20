fd417e5623c4766569d5d928487debf1dcf809b1
koena0912 <maitikoena@gmail.com>
2025-07-20 13:08:32 +0000

    Sync LeetCode submission - Water Bottles (java)

---
+++ problems/water_bottles/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int  ans=numBottles;
+        while(numBottles>=numExchange){
+            int newBottles=numBottles/numExchange;
+            int remainingBottles=numBottles%numExchange;
+            ans=ans+newBottles;
+            numBottles=newBottles+remainingBottles;
+        }
+        return ans;
+    }
+}
