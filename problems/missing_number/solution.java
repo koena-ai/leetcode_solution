4da0667492bb32af87b7d435b3899c9742614633
koena0912 <maitikoena@gmail.com>
2025-08-22 17:50:37 +0000

    Sync LeetCode submission - Missing Number (java)

--- problems/missing_number/solution.java
+++ problems/missing_number/solution.java
@@ -1,17 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int currentSum=0;
-        int range=nums.length;
-        int actualSum=(range*(range+1))/2;
-        
-        
-        for(int i=0;i<range;i++){
-            currentSum=currentSum+nums[i];
-        }
-        
-        int ans=actualSum - currentSum;
-        return ans;
+       int n=nums.length;
+       int s1=0;
+       for(int i=0;i<n;i++){
+        s1=s1+nums[i];
 
-
+       }
+       int s2=0;
+       s2=n*(n+1)/2;
+       int ans= s2-s1;
+       return ans;
     }
 }
