ae16002c80186b888aff6436f2486662329709df
koena0912 <maitikoena@gmail.com>
2025-09-13 03:26:47 +0000

    Sync LeetCode submission - Search Insert Position (java)

---
+++ problems/search_insert_position/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if(target<=nums[i]){
+             return i;   
+            }
+        
+    
+        }
+        return n;
+    }
+}
