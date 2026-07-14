82bad68a2c8552d253bd70c742647d42195f225d
koena0912 <maitikoena@gmail.com>
2026-07-14 14:21:27 +0000

    Sync LeetCode submission - Find Numbers with Even Number of Digits (java)

---
+++ problems/find_numbers_with_even_number_of_digits/solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution{
+    public int findNumbers(int [] nums){
+        int ans=0;
+        for(int num:nums){
+            if(String.valueOf(num).length()%2==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
