461a31b820b977534d0a59c8d900d8548cecb883
koena0912 <maitikoena@gmail.com>
2026-08-15 07:52:01 +0000

    Sync LeetCode submission - Set Matrix Zeroes (java)

---
+++ problems/set_matrix_zeroes/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      boolean[] zeroRows=new boolean[matrix.length];
+      boolean[] zeroCols=new boolean[matrix[0].length];
+      for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][j]==0){
+                zeroRows[i]=true;
+                zeroCols[j]=true;
+            }
+            
+        }
+      }  
+      for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+           if(zeroRows[i] || zeroCols[j]){
+               matrix[i][j]=0;
+      }   
+        }
+      }
+      
+    }
+}
